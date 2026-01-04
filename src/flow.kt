b00b63b96fd66cc11aa01ce0c7f9b581ce364c68
Voocke <timofeevi579@gmail.com>
2026-01-04 18:09:39 +0300

    flowOn and other

--- src/flow.kt
+++ src/flow.kt
@@ -1,17 +1,25 @@
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
 
 fun main() = runBlocking {
     val myFlow = makeFlow()
+        .filter { it % 2 == 0 }
+        .map { value -> "Получено число: $value" }
+        .flowOn(Dispatchers.IO)
+
     println("Flow создан, но ещё не запущен")
     delay(1000)
-    myFlow.collect { value -> println(value) }
+    myFlow.collect { value -> println("Collected $value on ${Thread.currentThread().name}") }
 }
 fun makeFlow(): Flow<Int> {
     return flow {
         println("флоу запущен")
         repeat(5){i ->
+            println("Emitting $i on ${Thread.currentThread().name}")
             emit(i)
             delay(1000)
         }
